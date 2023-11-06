22958c6ae5de35a2c2351022d0d14c77b358d575
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2023-11-06 13:15:20 -0500

    hooray

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,10 +1,28 @@
-import "../styles/style.css";
+import "../style/style.css";
 import javascriptLogo from "../javascript.svg";
-import viteLogo from "../public/vite.svg";
-import { setupCounter } from "../counter.js";
-// export exports scripts out and import imports in ^
-// don't forget npm i
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+import { gosling } from "./gosling.js";
 
+const DOMSelectors = {
+  theme1: document.querySelector(".theme1"),
+  theme2: document.querySelector(".theme2"),
+  movielist: document.querySelector(".movielist"),
+  post2015: document.querySelector(".post2015"),
+  dystopian: document.querySelector(".dystopian"),
+};
+const gosSelectors = {
+  name: gosling.forEach((i) => i.name),
+  genre: gosling.forEach((i) => i.genre),
+  release: gosling.forEach((i) => i.release),
+};
+DOMSelectors.movielist.innerHTML = `<div class="innercard">
+    <h1 class="name">${gosSelectors.name}</h1>
+    <p class="release"> released in ${gosSelectors.name}</p>
+    <p class="genre"> its genre is ${gosSelectors.genre}</p>
+  </div>`;
+
+/*
 document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -13,7 +31,7 @@ document.querySelector("#app").innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hello tihis webiet </h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
@@ -24,6 +42,4 @@ document.querySelector("#app").innerHTML = `
 `;
 
 setupCounter(document.querySelector("#counter"));
-
-const gosling = require("./gosling.json");
-console.log(gosling);
+ */
