faf4f6476edc9399edf6132e3e3df3b1c5ad7bf4
RichardM1337 <richardm246@nycstudents.net>
2023-11-20 17:56:03 -0500

    ...

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,6 @@
-import "../style/style.module.css";
-import "../style/background.module.css";
-import "../style/variables.module.css";
+import "../style/style.css";
+import "../style/background.css";
+import "../style/variables.css";
 import { gosling } from "./gosling.js";
 // images go in public folder, and can be freferred to globally
 const DOMSelectors = {
@@ -11,29 +11,33 @@ const DOMSelectors = {
   cardresetter: document.querySelector(".reset"),
 };
 
-gosling.forEach((i) => {
-  const card = document.createElement("div"); // creats div called card
-  card.innerHTML = `<div class="innercard">
-  <img src="${i.image}" class="movieimg" alt="${i.name}">
-  <h1 class="name">${i.name}</h1>
-  <p class="release"> released in ${i.release}</p>
-  <p class="genre"> its genre is ${i.genre}</p>
-  </div>`; // puts stuff
-  DOMSelectors.movielist.appendChild(card); // adds it into this biiiig div called movie list for my liking nyehehehehe
-  DOMSelectors.romance.addEventListener("click", function () {
-    if (i.genre != "Romance") {
-      card.style.display = "none";
-    }
+function createCard(arr) {
+  DOMSelectors.movielist.innerHTML = "";
+  arr.forEach((i) => {
+    const card = document.createElement("div"); // creats div called card
+    card.innerHTML = `<div class="innercard">
+    <img src="${i.image}" class="movieimg" alt="${i.name}">
+    <h1 class="name">${i.name}</h1>
+    <p class="release"> released in ${i.release}</p>
+    <p class="genre"> its genre is ${i.genre}</p>
+    </div>`; // puts stuff
+    DOMSelectors.movielist.appendChild(card);
   });
-  DOMSelectors.cardresetter.addEventListener("click", function () {
-    card.style.display = "block";
-  });
-});
-
-function destroy(item) {
-  item.innerHTML = "";
 }
 
+function filterMovies(userGenre) {
+  const gosFilter = gosling.filter((movie) => movie.genre.includes(userGenre));
+  createCard(gosFilter);
+}
+createCard(gosling);
+DOMSelectors.romance.addEventListener("click", function () {
+  filterMovies("Romance");
+});
+DOMSelectors.cardresetter.addEventListener("click", function () {
+  createCard(gosling);
+});
+
+/* CSS */
 DOMSelectors.darkcheck.addEventListener("click", function () {
   let body = document.body;
   if (body.classList.contains("lighttheme")) {
