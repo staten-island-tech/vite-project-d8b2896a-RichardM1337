b279773994ef2049bfabc7520548b39483ac8019
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2023-12-05 13:06:30 -0500

    blah blah

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -54,3 +54,33 @@ DOMSelectors.themepicker.addEventListener("change", function () {
     body.classList.add("lighttheme");
   }
 });
+
+// OLD CODE
+/*
+import "../style/style.module.css";
+import "../style/background.module.css";
+import "../style/variables.module.css";
+import { gosling } from "./gosling.js";
+// images go in public folder, and can be freferred to globally
+const DOMSelectors = {
+  darkcheck: document.querySelector(".darktheme"),
+  lightcheck: document.querySelector(".lighttheme"),
+  movielist: document.querySelector(".movielist"),
+  romance: document.querySelector(".romance"),
+  cardresetter: document.querySelector(".reset"),
+};
+gosling.forEach((i) => {
+  const card = document.createElement("div"); // creats div called card
+  card.innerHTML = `<div class="innercard">
+  <img src="${i.image}" class="movieimg" alt="${i.name}">
+  <h1 class="name">${i.name}</h1>
+  <p class="release"> released in ${i.release}</p>
+  <p class="genre"> its genre is ${i.genre}</p>
+  </div>`; // puts stuff
+  DOMSelectors.movielist.appendChild(card); // adds it into this biiiig div called movie list for my liking nyehehehehe
+  DOMSelectors.romance.addEventListener("click", function () {
+    if (i.genre != "Romance") {
+      card.style.display = "none";
+    }
+    */
+// I hid the unwanted cards instead of reconstructing my array
